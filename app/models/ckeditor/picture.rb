85e0496e98111fe3b9abaa454d7a0ae01c7a225b
krongk <kenrome@gmail.com>
2014-06-08 13:20:40 +0800

    Add thes

--- app/models/ckeditor/picture.rb
+++ app/models/ckeditor/picture.rb
@@ -1,8 +1,7 @@
 class Ckeditor::Picture < Ckeditor::Asset
   has_attached_file :data,
-                    :url  => "/ckeditor_assets/pictures/:id/:style.:extension",
-                    :path => ":rails_root/public/ckeditor_assets/pictures/:id/:style.:extension",
-                    :styles => { :content => '800>', :thumb => '270x180#' }
+                    :path => ":class/:id/:style.:extension",
+                    :styles => { :original => '640x640>', :thumb => '<320x320>' }
 
   validates_attachment_size :data, :less_than => 5.megabytes
   validates_attachment_presence :data
