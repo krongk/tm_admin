0c0f1588206d0346fb3315420b31c9db40c1006b
krongk <kenrome@gmail.com>
2014-11-15 00:24:01 +0800

    Fe

--- config/application.rb
+++ config/application.rb
@@ -33,6 +33,10 @@ module RainCms
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
+    #为了保证Ruby的时间和MySQL的时间一致，请不要设置time_zone,并设置以下内容：
+    # config.time_zone = 'Central Time (US & Canada)'
+    config.active_record.default_timezone = :local
+    config.active_record.time_zone_aware_attributes = false 
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
